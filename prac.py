d6670720cfc56e0d7e6557c1e2ffe26870a56e69
JakeZ34 <JzBaseball34@hotmail.com>
2022-04-04 16:07:52 -0700

    update

---
+++ prac.py
@@ -0,0 +1,8 @@
+def celciusToFahrenheit(cDegrees):
+    celsius = cDegrees
+    fahrenheit = (celsius * 1.8) + 32
+    return (fahrenheit)
+
+
+print(celciusToFahrenheit([0, 10, 100]))
+print("Hello World")
